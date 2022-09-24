7b4208c73bccab697bd387c6226c506f6af2c100
Baidevin <martinsdevin@gmail.com>
2022-09-25 00:26:05 +1000

    <Insert message here>

--- web-server/sketch.js
+++ web-server/sketch.js
@@ -1,10 +1,17 @@
 let points = [];
 let pendown = false
 let livemode = false
+
+let socket = new WebSocket("ws://localhost:8000");
+
+socket.onopen = function(e) {
+  console.log("SHE WORKS BWOIIIIIIIIII")
+};
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(220);
-  
+  // socket.emit("test")
 }
 
 function draw() {
