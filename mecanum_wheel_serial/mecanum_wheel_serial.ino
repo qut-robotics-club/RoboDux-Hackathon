383817a51c926be94b1d5a8b8d1da4b7a63540cd
bb3b19-s905b <rtmalone1@outlook.com>
2022-09-26 02:13:41 +1000

    Arduino Mecanum Control and Serial Read

---
+++ mecanum_wheel_serial/mecanum_wheel_serial.ino
@@ -0,0 +1,57 @@
+double v1 = 0; // Front Left
+double v2 = 0; // Back Right
+double v3 = 0; // Front Left
+double v4 = 0; // Back Right
+
+double angle = 0;
+double velocity = 0;
+
+int i = 0;
+String msg[2];
+
+String temp;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //if (Serial.available() > 0) {
+    //String data = Serial.readStringUntil('\n');    
+  //}
+  i = 0;
+  msg[0] = msg[1] = "";
+  if (Serial.available()) {
+    delay(10);
+    while (Serial.available() > 0) {
+      temp = (char)Serial.read();
+
+      if (temp == ",") {
+        i += 1;
+      }
+      
+      msg[i] += temp;
+    }
+    Serial.flush();
+
+    drive(msg);
+  }
+}
+
+void drive(String msg[]) {
+
+  velocity = msg[0].toDouble();
+  angle = msg[1].toDouble();
+  
+  v1 = velocity * cos(angle);
+  v2 = velocity * sin(angle);
+  v3 = velocity * sin(angle);
+  v4 = velocity * cos(angle);
+
+  v1 = map(v1, -velocity, velocity, -100, 100);
+  v2 = map(v2, -velocity, velocity, -100, 100);
+  v3 = map(v3, -velocity, velocity, -100, 100);
+  v4 = map(v4, -velocity, velocity, -100, 100);
+}
